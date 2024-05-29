d840b11d359c48eb3dfe1af55dd7e1c17776812b
Patrik Cesnek <pcesnek290@gmail.com>
2024-05-29 14:31:10 +0200

    Make skeleton prettier

--- TicTacToe/Extensions/SkeletonView.swift
+++ TicTacToe/Extensions/SkeletonView.swift
@@ -1,28 +1,43 @@
-//
-//  SkeletonView.swift
-//  TicTacToe
-//
-//  Created by Patrik Cesnek on 27/05/2024.
-//
-
 import SwiftUI
 
 struct BlinkViewModifier: ViewModifier {
     let duration: Double
+    let blinkFrequency: Double
     @State private var blinking: Bool = false
-
+    @State private var startBlinking: Bool = true
+    
     func body(content: Content) -> some View {
         content
-            .opacity(blinking ? 0.3 : 1)
-            .animation(.easeInOut(duration: duration).repeatForever(autoreverses: true), value: blinking)
+            .opacity(blinking ? 0.4 : 0.8)
+            .animation(
+                Animation.easeInOut(duration: 1 / blinkFrequency)
+                    .repeatForever(autoreverses: true),
+                value: blinking
+            )
             .onAppear {
-                blinking.toggle()
+                startBlinkingAnimation()
             }
+    }
+    
+    private func startBlinkingAnimation() {
+        blinking = true
+        DispatchQueue.main.asyncAfter(deadline: .now() + duration) {
+            startBlinking = false
+        }
+        
+        Timer.scheduledTimer(withTimeInterval: 1 / blinkFrequency, repeats: true) { timer in
+            if startBlinking {
+                blinking.toggle()
+            } else {
+                timer.invalidate()
+                blinking = false
+            }
+        }
     }
 }
 
 extension View {
-    func blinking(duration: Double = 1) -> some View {
-        modifier(BlinkViewModifier(duration: duration))
+    func blinking(duration: Double, blinkFrequency: Double = 2.5) -> some View {
+        self.modifier(BlinkViewModifier(duration: duration, blinkFrequency: blinkFrequency))
     }
 }
